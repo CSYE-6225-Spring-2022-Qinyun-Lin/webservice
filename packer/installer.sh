117e3cc5911372960276621f0bf730e22a1c3177
Qinyun-Lin <linqinyun@outlook.com>
2022-03-02 20:28:24 -0500

    feat: auto deployment

--- packer/installer.sh
+++ packer/installer.sh
@@ -1,45 +1,70 @@
 #!/bin/bash
 
-sudo yum -y update
+sleep 10
+export http_proxy= $Write proxy
+export https_proxy= $write proxy
 
-sudo yum -y install wget
+sudo -E yum -y update
 
-sudo wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+cd ~
+mkdir webservice
+cp /tmp/webservice.zip ~/webservice/webservice.zip
 
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo -E yum -y install unzip
 
-sudo rpm -Uvh mysql80-community-release-el7-3.noarch.rpm
+cd webservice
+unzip webservice.zip
+ls -al
+pwd
 
-sudo yum install mysql-server -y
+sudo -E yum -y install wget
+wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 
-sleep 10s
+sudo  rpm -ivh mysql-community-release-el7-5.noarch.rpm
+sudo -E yum -y update
+sudo -E yum -y install mysql-server
+sudo systemctl start mysqld
+sleep 1s
 
-sudo systemctl start mysqld
-sudo systemctl start mysqld.service
-sudo service mysqld.service start
-
-sudo mysql --version
-
-cd /var/log
-temp=`sudo grep 'password' ./mysqld.log | gawk -F 'localhost: ' '{print $2}'`
-
-echo $temp
-
+# cd /var/log
+# temp=`sudo grep 'password' ./mysqld.log | gawk -F 'localhost: ' '{print $2}'`
+# echo $temp
 # mysql -u root -p$temp --connect-expired-password -c "Alter user 'root'@'localhost' IDENTIFIED with mysql_native_password BY 'admin';"
 
-mysql -u root -p$temp <<-EOF
-UPDATE mysql.user SET Password=PASSWORD('admin') WHERE User='root';
+mysql -u root <<-EOF
+UPDATE mysql.user SET Password=PASSWORD('adminadmin!') WHERE User='root';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 FLUSH PRIVILEGES;
 CREATE DATABASE csye6225;
+use csye6225;
+create table health
+(
+    id varchar(50) not null,
+    user_name varchar(50) not null,
+    password varchar(100) not null,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    account_created datetime not null,
+    account_updated datetime not null,
+    constraint heath_id_uindex
+        unique (id),
+    constraint heath_user_name_uindex
+        unique (user_name)
+);
+
+alter table health
+    add primary key (id);
 EOF
 
 sudo yum -y install python3
-
 sudo yum -y install python3-pip
-
 sudo pip3 install flask mysql-connector
 
-echo all done!
+cp ~/webservice/systemd/webapp.service /etc/systemd/system/webapp.service
+sudo chmod 644 /etc/systemd/system/webapp.service
+sudo -E yum install systemd -y
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
