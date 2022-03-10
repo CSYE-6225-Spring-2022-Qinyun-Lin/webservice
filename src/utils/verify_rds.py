f703799d5e2c00d66e521ac6c8328393ea4bb988
Qinyun-Lin <linqinyun@outlook.com>
2022-03-10 00:56:54 -0500

    feat: delete old img when updating & script to verify RDS content

---
+++ src/utils/verify_rds.py
@@ -0,0 +1,30 @@
+import mysql.connector as mc
+
+
+host = open("/home/ec2-user/webConfig/mysql_host.txt").readline().strip()
+user, password = open("/home/ec2-user/webConfig/mysql_key.txt").readline().strip().split(", ")
+
+
+def get_all():
+    sql = "select * from health"
+    config = {
+        'user': user,
+        'password': password,
+        'host': host,
+        'port': '3306',
+        'database': 'csye6225'
+    }
+    con = mc.connect(**config)
+
+    cursor = con.cursor(buffered=True)
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+        con.close()
+    except Exception:
+        return None
+
+
+if __name__ == '__main__':
+    get_all()
